fe5b2117333e0c1e0c6d893d9c2493f67a6c91c1
nebula682 <malcolmnie@gmail.com>
2023-09-15 05:20:41 -0700

    loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //getting the magic number
+        Console.WriteLine(" What is your magic number? " );
+        int magicNumber = int.Parse(Console.Readline());
+
+        // now using a random number
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        //alternative to use a do whileloop
+
+        while (guess!= magicNumber)
+    }
+
+    {
+
+        // getting the guess from user
+
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        // using loops to know whether its higher or lower
+        
+        if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
+    }
+}
+
+
+
+
+
     }
 }
